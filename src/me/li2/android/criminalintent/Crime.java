194d1a39abdf0b6139a8d7d784daeb15b981cc9f
WeiYi.Li <weiyi.just2@gmail.com>
2015-03-28 14:01:48 +0800

    添加更多的实例变量到Crime类，同时更新Fragment布局，并使用新添加的组件

--- src/me/li2/android/criminalintent/Crime.java
+++ src/me/li2/android/criminalintent/Crime.java
@@ -1,15 +1,19 @@
 package me.li2.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
     
     public Crime() {
         // 生成惟一的标识符
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public String getTitle() {
@@ -22,5 +26,21 @@ public class Crime {
 
     public UUID getId() {
         return mId;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
     }
 }
--- src/me/li2/android/criminalintent/CrimeFragment.java
+++ src/me/li2/android/criminalintent/CrimeFragment.java
@@ -7,11 +7,17 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,20 +31,29 @@ public class CrimeFragment extends Fragment {
         
         mTitleField  = (EditText) v.findViewById(R.id.crime_title);
         mTitleField.addTextChangedListener(new TextWatcher() {
-            
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 mCrime.setTitle(s.toString());
             }
-            
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count,  int after) {
                 // This space intentionally left blank
             }
-            
             @Override
             public void afterTextChanged(Editable s) {
                 // This one too
+            }
+        });
+        
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+        
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
             }
         });
         
