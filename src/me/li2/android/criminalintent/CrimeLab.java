e1bb1d2396674eb2d94192ee97975f974d97ccc0
WeiYi.Li <weiyi.just2@gmail.com>
2015-03-29 00:17:52 +0800

    建立ListView内容的模型层： (1)使用ArrayList<?>存储所有的列表项内容； (2)考虑到数据只需在内存中保存一份，使用单例singleton集中存储数据，通过私有构造器方法和get()方法返回static变量来构建单例。

---
+++ src/me/li2/android/criminalintent/CrimeLab.java
@@ -0,0 +1,44 @@
+package me.li2.android.criminalintent;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import android.content.Context;
+
+public class CrimeLab {
+    // Singletons and centralized data storage
+    private static CrimeLab sCrimeLab;
+    private ArrayList<Crime> mCrimes; 
+    private Context mAppContex;
+    
+    private CrimeLab(Context appContext) {
+        mAppContex = appContext;
+        for (int i=0; i<100; i++) {
+            Crime c = new Crime();
+            c.setTitle("Crime #" + i);
+            c.setSolved(i % 2 == 0);
+            mCrimes.add(c);
+        }
+    }
+    
+    // Setting up the singleton
+    public static CrimeLab get(Context c) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(c.getApplicationContext());
+        }
+        return sCrimeLab;
+    }
+    
+    public ArrayList<Crime> getCrimes() {
+        return mCrimes;
+    }
+    
+    public Crime getCrime(UUID id) {
+        for (Crime c : mCrimes) {
+            if (c.getId() .equals(id)) {
+                return c;
+            }
+        }
+        return null;
+    }
+}
