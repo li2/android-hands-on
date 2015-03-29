ed03010e1f5289963d101448041740d9090e4e08
WeiYi.Li <weiyi.just2@gmail.com>
2015-03-29 11:36:53 +0800

    使用抽象FragmentActivity托管Fragment (1)如果存在比较多的情形，这些activity只管理一个fragment，那么托管fragment的代码几近通用，就可以把这些activity抽象出来，命名为`SingleFragmentActivity.java`，它的布局文件命名为`activity_fragment.xml`。 (2)这个类惟一不同用的是实例化fragment，所以新增了一个名为createFragment()的抽象方法，使用它实例化新的fragment。 (3)所以SingleFragmentActivity的子类需要实现该方法，返回一个由activity托管的fragment实例。

---
+++ src/me/li2/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,18 @@
+package me.li2.android.criminalintent;
+
+import java.util.ArrayList;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+
+public class CrimeListFragment extends ListFragment{
+    private ArrayList<Crime> mCrimes;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActivity().setTitle(R.string.crimes_title);
+        mCrimes = CrimeLab.get(getActivity()).getCrimes();
+    }
+
+}
---
+++ src/me/li2/android/criminalintent/SingleFragmentActivity.java
@@ -0,0 +1,30 @@
+package me.li2.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+public abstract class SingleFragmentActivity extends FragmentActivity {
+
+    protected abstract Fragment createFragment();
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+        
+        // Get the FragmentManager.
+        FragmentManager fm = getSupportFragmentManager();
+        // Ask the FragmentManager for the fragment with a container view ID, 
+        // If this fragment is already in the list, the FragmentManager will return it,
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+        
+        // Or create a new CrimeFragment,
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            // Create a new fragment transaction, include one add operation in it, and then commit it.
+            fm.beginTransaction().add(R.id.fragmentContainer, createFragment()).commit();
+        }
+    }
+}
