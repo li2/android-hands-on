5a572dc4b4a8e44ccd0c01fb5212e83fb3647a65
WeiYi.Li <weiyi.just2@gmail.com>
2015-04-04 17:36:28 +0800

    同一个activity托管的两个fragment之间发送和回传数据 (1)从ActivityA托管的fragmentA中启动ActivityB，而ActivityB也托管着一个fragmentB，所以需要在fragmentB中返回数据，fragment没有setResult方法，但可以先取得托管它的activity，然后再返回数据。 每次启动一个activity时都会绑定一个请求码，而被启动的activity返回数据时只需setResult(int resultCode, Intent data)；由负责启动的activity区分是谁返回的数据。 （具体代码在提交版本“通过 fragment 获取返回结果”）
    (2)而同一个activity托管的两个fragment之间，如何发送和回传数据？我们只能是填充好一个fragment视图，然后把fragment视图放到托管它的activity视图内，没有“start fragment”这个概念。
    但fragment提供来了另外一种绑定方式：为fragment设置数据返回的目标fragment和请求码：调用Fragment.setTargetFragment(Fragment fragment, int requestCode)。
    
    然后通过getTargetFragment().onActivityResult(getTargetRequestCode(), int resultCode, Intent data)方法实现数据的回传。
    
    (3)返回数据时的区别：ActivityManager会自动调用Activity.onActivityResult(...)方法，父activity接收到Activity.onActivityResult(...)方法的调用后，其FragmentManager会调用对应fragment的Fragment.onActivityResult(...)方法。
    处理由同一activity托管的两个fragment间的数据返回时，可借用Fragment.onActivityResult(...)方法。

--- src/me/li2/android/criminalintent/CrimeFragment.java
+++ src/me/li2/android/criminalintent/CrimeFragment.java
@@ -1,8 +1,10 @@
 package me.li2.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -25,6 +27,7 @@ public class CrimeFragment extends Fragment {
     public static final String EXTRA_CRIME_ID = "me.li2.android.criminalintent.crime_id";
     
     private static final String DIALOG_DATE = "date";
+    private static final int REQUEST_DATE = 0;
     
     private Crime mCrime;
     private EditText mTitleField;
@@ -77,12 +80,13 @@ public class CrimeFragment extends Fragment {
         });
         
         mDateButton = (Button) v.findViewById(R.id.crime_date);
-        mDateButton.setText(mCrime.getDate().toString());
+        updateDate();
         mDateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FragmentManager fm = getActivity().getSupportFragmentManager();
                 DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
+                dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
                 dialog.show(fm, DIALOG_DATE);
             }
         });
@@ -101,5 +105,21 @@ public class CrimeFragment extends Fragment {
     
     public void returnResult() {
         getActivity().setResult(Activity.RESULT_OK, null);
+    }
+    
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+        if (requestCode == REQUEST_DATE) {
+            Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mCrime.setDate(date);
+            updateDate();
+        }
+    }
+    
+    public void updateDate() {
+        mDateButton.setText(mCrime.getDate().toString());
     }
 }
--- src/me/li2/android/criminalintent/DatePickerFragment.java
+++ src/me/li2/android/criminalintent/DatePickerFragment.java
@@ -4,8 +4,11 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
@@ -25,6 +28,16 @@ public class DatePickerFragment extends DialogFragment {
         fragment.setArguments(args);
         
         return fragment;
+    }
+    
+    private void sendResult(int resultCode) {
+        if (getTargetFragment() == null) {
+            return;
+        }
+        
+        Intent i = new Intent();
+        i.putExtra(EXTRA_DATE, mDate);
+        getTargetFragment().onActivityResult(getTargetRequestCode(), resultCode, i);
     }
     
     @Override
@@ -60,7 +73,13 @@ public class DatePickerFragment extends DialogFragment {
         return new AlertDialog.Builder(getActivity())
             .setView(v)
             .setTitle(R.string.date_picker_title)
-            .setPositiveButton(android.R.string.ok, null)
+            .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    sendResult(Activity.RESULT_OK);
+                }
+            })
             .create();
     }
+
 }
