ccc608200d5a623feaf45d4c0b9981dbc926c607
WeiYi.Li <weiyi.just2@gmail.com>
2015-04-12 15:36:13 +0800

    显示actionbar需要使用`android:style/Theme.Holo.Light`作为app theme.

--- src/me/li2/android/criminalintent/CrimeListActivity.java
+++ src/me/li2/android/criminalintent/CrimeListActivity.java
@@ -1,9 +1,22 @@
 package me.li2.android.criminalintent;
 
+import android.app.ActionBar;
 import android.support.v4.app.Fragment;
 
 public class CrimeListActivity extends SingleFragmentActivity {
+    private static final String TAG = "CrimeListActivity";
 
+    @Override
+    protected void onCreate(android.os.Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar actionBar = getActionBar();
+        // should use @android:style/Theme.Holo.Light as app theme,
+        // Theme.AppCompat.Light without action bar.
+        if (actionBar != null) {
+            actionBar.setTitle(getResources().getString(R.string.app_name));
+        }
+    }
+    
     @Override
     protected Fragment createFragment() {
         return new CrimeListFragment();
