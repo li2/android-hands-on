87a5e1579866117e319c24ef42f521960d42181f
weiyi.li <weiyi.just2@gmail.com>
2015-09-15 15:57:54 +0800

    Setup a custom GridView Adapter to show ImageView instead of default TextView.

--- PhotoGallery/src/me/li2/android/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/src/me/li2/android/photogallery/PhotoGalleryFragment.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 public class PhotoGalleryFragment extends Fragment {
     private static final String TAG = "PhotoGalleryFragment";
@@ -38,8 +39,7 @@ public class PhotoGalleryFragment extends Fragment {
             return;
         }
         if (mItems != null) {
-            mGridView.setAdapter(new ArrayAdapter<GalleryItem>(getActivity(),
-                    android.R.layout.simple_gallery_item, mItems));
+            mGridView.setAdapter(new GalleryItemAdapter(mItems));
         } else {
             mGridView.setAdapter(null);
         }
@@ -57,5 +57,21 @@ public class PhotoGalleryFragment extends Fragment {
             mItems = items;
             setupAdapter();
         }
+    }
+    
+    private class GalleryItemAdapter extends ArrayAdapter<GalleryItem> {
+        public GalleryItemAdapter(ArrayList<GalleryItem> items) {
+            super(getActivity(), 0, items);
+        }
+        
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getActivity().getLayoutInflater().inflate(R.layout.gallery_item, parent, false);
+            }
+            ImageView imageView = (ImageView) convertView.findViewById(R.id.gallery_item_imageView);
+            imageView.setImageResource(R.drawable.ic_photo);
+            return convertView;
+        }        
     }
 }
