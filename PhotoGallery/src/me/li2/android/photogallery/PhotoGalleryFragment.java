6d23b7a4d03cb610cc0f1dae528dad9f945fdd13
weiyi.li <weiyi.just2@gmail.com>
2015-09-15 14:32:37 +0800

    Setup a simple GridView adapter in an AsyncTask.

--- PhotoGallery/src/me/li2/android/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/src/me/li2/android/photogallery/PhotoGalleryFragment.java
@@ -1,19 +1,20 @@
 package me.li2.android.photogallery;
 
-import java.io.IOException;
+import java.util.ArrayList;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
 public class PhotoGalleryFragment extends Fragment {
     private static final String TAG = "PhotoGalleryFragment";
     GridView mGridView;
+    ArrayList<GalleryItem> mItems;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,15 +27,35 @@ public class PhotoGalleryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
         mGridView = (GridView) view.findViewById(R.id.gridView);
+        setupAdapter();
         return view;
     }
     
+    void setupAdapter() {
+        // when using an AsyncTask, you must check to make sure that your fragment is still attached.
+        // If it is not, then operations that rely on the that activity (like creating your ArrayAdapter) will fail. 
+        if (getActivity() == null || mGridView == null) {
+            return;
+        }
+        if (mItems != null) {
+            mGridView.setAdapter(new ArrayAdapter<GalleryItem>(getActivity(),
+                    android.R.layout.simple_gallery_item, mItems));
+        } else {
+            mGridView.setAdapter(null);
+        }
+    }
+    
     // Using AsyncTask to run on a background thread.
-    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+    private class FetchItemsTask extends AsyncTask<Void, Void, ArrayList<GalleryItem>> {
         @Override
-        protected Void doInBackground(Void... params) {
-            new FlickrFetchr().fetchItems();
-            return null;
+        protected ArrayList<GalleryItem> doInBackground(Void... params) {
+            return new FlickrFetchr().fetchItems();
+        }
+        
+        @Override
+        protected void onPostExecute(ArrayList<GalleryItem> items) {
+            mItems = items;
+            setupAdapter();
         }
     }
 }
